c3dd883122ccc407f2549c310efe3307dadb54f2
yovodeviz <yovodevizaideteko@outlook.fr>
2023-10-18 09:57:54 +0200

    navbar

--- commons/navbar.php
+++ commons/navbar.php
@@ -10,11 +10,18 @@
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Accueil</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link dropdown-toggle" href="#">Technologies</a>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Technologies</a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Capteurs</a></li>
+                    <li><a class="dropdown-item" href="#">Moteurs</a></li>
+                    <li><a class="dropdown-item" href="#">Electronique</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Application</a></li>
+                </ul>
               </li>
               <li class="nav-item">
-                <a class="nav-link dropdown-toggle" href="#">Groupe</a>
+                <a class="nav-link groupe-toggle" href="#">Groupe</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Quizz</a>

b82c9f2018e6e08e8b1f10aab73e8e79bc80e139
Herve <herve.eruam@etu.univ-savoie.fr>
2023-10-18 10:20:49 +0200

    avancement index.html

--- index.php
+++ index.php
@@ -1,33 +1,53 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
+
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-
 <body>
-<?php
-include("commons/navbar.php")
-?>
 
+<?php include("commons/navbar.php") ?>
 
-<!-- Optional JavaScript; choose one of the two! -->
+<div id="main" class="container">
+    <div id="titre" class="text-center">
+        <h1>Projet d'études: Volet battant connecté et intelligent</h1>
+    </div>
 
+    <div id="video" class="text-center">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ftD44t1jq8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
 
-<!-- Option 1: Bootstrap Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <div id="soustitre1">
+        <h2>Volets battants connectés</h2>
+    </div>
 
-<!-- Option 2: Separate Popper and Bootstrap JS -->
-<!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
--->
+    <div id="intro">
+        <p>Les volets battants motorisés sont la première étape pour rendre la vie à la maison plus confortable et plus pratique. En ajoutant une touche de connectivité, les volets battants augmentent la sécurité de votre maison, optimisent l’efficacité énergétique et facilitent votre vie quotidienne.</p>
+    </div>
+
+    <div id="soustitre2">
+        <h2>3 bonnes raisons d'installer des volets battants automatisés chez vous:</h2>
+    </div>
+
+    <div id="liste">
+        <ul>
+            <li>Se libérer des tâches répétitives en regroupant facilement vos commandes de volets avec une commande intelligente en un simple geste, avec votre smartphone ou avec un assistant vocal. Programmez l’ouverture et la fermeture automatiquement et améliorez votre confort.</li>
+            <li>Optimisez l'isolation de votre maison et votre confort énergétique quelle que soit la saison. En hiver, programmez l'ouverture et la fermeture de vos volets battants connectés pour conserver la chaleur à l’intérieur et réduire vos dépenses énergétiques. En été, programmez un seuil de température pour fermer automatiquement vos volets battants électriques. La fraîcheur reste à l'intérieur de votre maison.</li>
+            <li>Vos volets sont les premiers remparts de votre maison. Vous avez même la possibilité de fermer à distance vos volets battants connectés via l’application.</li>
+        </ul>
+    </div>
+
+    <embed src="../documents/presentation_S6.pdf" width="100%" height="45%" type="application/pdf"/>
+
+    <div id="logo" class="text-center">
+        <img src="../documents/photos/logopopo.png" width="400" class="img-fluid">
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
